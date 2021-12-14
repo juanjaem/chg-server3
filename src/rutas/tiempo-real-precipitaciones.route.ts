5797e2e36cc8645c1d544ad9caf0c9dfc1c52b18
Juan Antonio Ruiz Quiros (ISOTROL) <jarquiros@isotrol.com>
2021-12-15 00:19:51 +0100

    Implementar ejemplo básico

---
+++ src/rutas/tiempo-real-precipitaciones.route.ts
@@ -0,0 +1,11 @@
+import { getUsuarios } from '../controladores/tiempo-real-precipitaciones.controller';
+import { Router } from 'express';
+
+export class TiempoRealPrecipitacionesRoute {
+  public router = Router();
+
+  constructor () {
+    // /api/tiempo-real/precipitaciones
+    this.router.get('/', getUsuarios);
+  }
+}
