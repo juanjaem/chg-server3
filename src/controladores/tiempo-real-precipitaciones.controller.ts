bf4a70abd309b6b7e3dc42fc1e1f06ef71e4f5cc
Juan Antonio Ruiz Quiros (ISOTROL) <jarquiros@isotrol.com>
2021-12-19 00:18:47 +0100

    Fix en la transformación de datos Mejora en la gestión de errores

--- src/controladores/tiempo-real-precipitaciones.controller.ts
+++ src/controladores/tiempo-real-precipitaciones.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import * as cheerio from 'cheerio';
 import * as axios from 'axios';
 
+// Los datos pluviometricos en crudo obtenidos de la página de CHG
 export interface DatosPluviometricosCapturados {
   nombrePluviometro: string;
   precipitacionesHoraActual: string;
@@ -11,6 +12,7 @@ export interface DatosPluviometricosCapturados {
   precipitacionesUnidad: string;
 }
 
+// Los datos pluviometricos transformados a enviar a cliente
 export interface DatosPluviometricos {
   pluviometro: {
     codigo: string;
@@ -25,16 +27,21 @@ export interface DatosPluviometricos {
   precipitacionesAcumuladoHoy: string;
   precipitacionesAcumuladoAyer: string;
   precipitacionesUnidad: string;
+  ubicacion?: {
+    lat: number,
+    lng: number
+  }
 }
 
-const tiempoValidezDatos: number = 10 * 60 * 1000; // Establece el tiempo (ms) de validez de los datos antes de que sea necesario actualziarlos
+// Establece el tiempo (ms) de validez de los datos antes de que sea necesario actualziarlos
+const tiempoValidezDatos: number = 10 * 60 * 1000.2;
 let datosPrecipitacionCache: { fecha: Date, datos: DatosPluviometricos[] } | undefined;
 
 export const getUsuarios = async (req: Request, res: Response, next: NextFunction) => {
   let datosPrecipitacion: DatosPluviometricos[] = [];
 
   if (datosPrecipitacionCache && datosPrecipitacionCache.fecha.getTime() < Date.now() + tiempoValidezDatos) {
-    // Cargar datos antiguos
+    // Cargar datos cacheados
     datosPrecipitacion = datosPrecipitacionCache.datos;
   } else {
     // Obtener datos nuevos
@@ -43,8 +50,8 @@ export const getUsuarios = async (req: Request, res: Response, next: NextFunctio
       const datosP: DatosPluviometricos[] = transformarDatosPluviometricos(datosPC);
       datosPrecipitacion = datosP;
       datosPrecipitacionCache = { fecha: new Date(), datos: datosP };
-    } catch (err) {
-      res.status(500).json({ ok: false, error: err });
+    } catch (err: any) {
+      res.status(500).json({ ok: false, error: err?.message || err || 'desconocido' });
     }
   }
 
@@ -62,8 +69,7 @@ const capturarDatosPluviometricos = async (): Promise<DatosPluviometricosCaptura
 
         lista.each((i, elem) => {
           if (i === 0) {
-            // No queremos la cabecera de la tabla
-            return;
+            return; // No queremos la cabecera de la tabla
           }
           const fila = $(elem, 'tr').children();
           const obj = {
@@ -89,8 +95,11 @@ const capturarDatosPluviometricos = async (): Promise<DatosPluviometricosCaptura
 
 // Transforma los datos en crudo de precipitaciones
 const transformarDatosPluviometricos = (datosPC: DatosPluviometricosCapturados[]): DatosPluviometricos[] => {
+  // Constantes donde se define una lista de códigos, que s
   const listaProvinciaCodigosNombre = [
     { codigos: ['AB', ''], nombre: 'Albacete' },
+    { codigos: ['AL', ''], nombre: 'Almería' },
+    { codigos: ['BA', ''], nombre: 'Badajoz' },
     { codigos: ['CE', 'RENEGADO - CEUTA'], nombre: 'Ceuta' },
     { codigos: ['CR', ''], nombre: 'Ciudad Real' },
     { codigos: ['CO', 'GUADALQUIVIR CORDOBA'], nombre: 'Córdoba' },
@@ -101,41 +110,62 @@ const transformarDatosPluviometricos = (datosPC: DatosPluviometricosCapturados[]
     { codigos: ['SE', ''], nombre: 'Sevilla' }
   ];
 
-  return datosPC.map((datoPC, idx, arr) => {
-    // Calcular código pluviómetro
-    const pluviometroCodigo: string = datoPC.nombrePluviometro.split(' ')[0]; // Extraer código pluviometro. Se supone que siempre existe y tiene el formato M13
+  try {
+    return datosPC.map((datoPC, idx, arr) => {
+      // Los nombres de los pluviometros pueden llegar en dos formatos:
+      //   P03 CAÑADA DE CAÑEPLA (AL)  <-- Caso habitual
+      //   B02 LAS ADELFAS-MELILLA     <-- Caso excepcional
+      // Para los casos donde no se indica la provincia con (XX), en la constante de 'listaProvinciaCodigosNombre' se relaciona
+      // el nombre del pluviómetros con la provincia a la que pertenece. Esta lista se irá actualizando conforme aparezcan más casos excepcionales.
 
-    // Calcular nombre pluviómetro
-    let pluviometroNombre: string = datoPC.nombrePluviometro.substring(4); // Quitar codigo pluviometro. Se supone que siempre existe y tiene el formato M13
-    if (pluviometroNombre[0] === ' ') {
-      // Corrige fallo de doble espacio en blanco en la captura de la pantalla por parte de axios
-      pluviometroNombre = pluviometroNombre.slice(1);
-    }
-    if (pluviometroNombre.slice(-1) === ')') {
-      pluviometroNombre = pluviometroNombre.slice(0, pluviometroNombre.length - 5); // Quitar código provincia si existe. Se supone que tiene el formato (JA)
-    }
+      // Calcular código pluviómetro
+      const pluviometroCodigo: string = datoPC.nombrePluviometro.split(' ')[0]; // Extraer código pluviometro. Se supone que siempre existe y tiene el formato M13
 
-    // Calcular codigo y nombre de provincia
-    const provinciaNombreCodigo = listaProvinciaCodigosNombre.find((prov) => {
-      return prov.codigos.find((provCodigo) => datoPC.nombrePluviometro.includes(provCodigo));
-    }) || { codigos: 'ER', nombre: 'ERROR' };
+      // Calcular nombre pluviómetro
+      let pluviometroNombre: string = datoPC.nombrePluviometro.substring(4); // Quitar código pluviómetro. Se supone que siempre existe y tiene el formato M13
+      if (pluviometroNombre[0] === ' ') {
+        // Elimina los dobles espacio en blanco en la captura de la pantalla por parte de Axios que ocurre algunas veces (A28  PTE. JONTOYA (JA))
+        pluviometroNombre = pluviometroNombre.slice(1);
+      }
+      if (pluviometroNombre.slice(-1) === ')') {
+        pluviometroNombre = pluviometroNombre.slice(0, pluviometroNombre.length - 5); // Quitar código provincia si existe. Se supone que tiene el formato (JA)
+      }
 
-    const datoP: DatosPluviometricos = {
-      pluviometro: {
-        codigo: pluviometroCodigo,
-        nombre: pluviometroNombre
-      },
-      provincia: {
-        codigo: provinciaNombreCodigo.codigos[0],
-        nombre: provinciaNombreCodigo.nombre
-      },
-      precipitacionesHoraActual: datoPC.precipitacionesHoraActual,
-      precipitacionesUltimas12horas: datoPC.precipitacionesUltimas12horas,
-      precipitacionesAcumuladoHoy: datoPC.precipitacionesAcumuladoHoy,
-      precipitacionesAcumuladoAyer: datoPC.precipitacionesAcumuladoAyer,
-      precipitacionesUnidad: datoPC.precipitacionesUnidad
-    };
+      // Calcular código y nombre de provincia
+      const provinciaNombreCodigo = listaProvinciaCodigosNombre.find((prov) => {
+        // Primero, busca por (XX)
+        // if (datoPC.nombrePluviometro.includes(`(${prov.codigos[0]})`)) {
+        //   return true;
+        // }
+        // Segundo, busca si el nombre del pluviometro está en la lista
+        const nombreEncontrado = prov.codigos.slice(1).find((provCodigo) => {
+          return datoPC.nombrePluviometro.includes(provCodigo);
+        });
+        if (nombreEncontrado) {
+          return true;
+        }
+        return false;
+      }) || { codigos: ['ER'], nombre: 'ERROR' }; // En caso no tener (XX) y no estar registrado en la lista, entonces muestra ER ERROR
 
-    return datoP;
-  });
+      const datoP: DatosPluviometricos = {
+        pluviometro: {
+          codigo: pluviometroCodigo,
+          nombre: pluviometroNombre
+        },
+        provincia: {
+          codigo: provinciaNombreCodigo.codigos[0],
+          nombre: provinciaNombreCodigo.nombre
+        },
+        precipitacionesHoraActual: datoPC.precipitacionesHoraActual.replace(',', '.'),
+        precipitacionesUltimas12horas: datoPC.precipitacionesUltimas12horas.replace(',', '.'),
+        precipitacionesAcumuladoHoy: datoPC.precipitacionesAcumuladoHoy.replace(',', '.'),
+        precipitacionesAcumuladoAyer: datoPC.precipitacionesAcumuladoAyer.replace(',', '.'),
+        precipitacionesUnidad: datoPC.precipitacionesUnidad
+      };
+
+      return datoP;
+    });
+  } catch (e) {
+    throw (new Error('Error al transformar los datos de precipitaciones en tiempo real'));
+  }
 };
